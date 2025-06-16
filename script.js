f0affa4caad2dfd3c9653a1e5793d8c4641b1145
Jeremy Chan <jeremynchan@gmail.com>
2025-06-16 13:05:10 -0700

    fix resume

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Initialize animations
 AOS.init({ duration: 800, once: true });
 
-// Grab clickable links & content container
-const links = document.querySelectorAll('.main-nav a');
+// Grab clickable links & content container, skip external links
+const links = document.querySelectorAll('.main-nav a:not(.external-link');
 const container = document.getElementById('content');
 
 async function loadSection(route, addToHistory = true) {
